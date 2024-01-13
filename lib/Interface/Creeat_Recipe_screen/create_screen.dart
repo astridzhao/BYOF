41dbf7d3664ac203507ab9b6ce7eae49923ee0ee
Astrid Ziyin Zhao <79214456+astridzhao@users.noreply.github.com>
2024-01-13 14:47:25 -0500

    remove key (#2)

--- lib/Interface/Creeat_Recipe_screen/create_screen.dart
+++ lib/Interface/Creeat_Recipe_screen/create_screen.dart
@@ -424,10 +424,9 @@ class CreateScreenState extends State<CreateScreen> {
   sendPrompt() async {
     OpenAI.apiKey = apiKey;
     final systemMessage = OpenAIChatCompletionChoiceMessageModel(
-      content:
-          "As a recipe-generating assistant, your role is to create a recipe based on the" +
-              selectedIngredients.join() +
-              "provided by the user. To ensure a precise and high-quality response, please follow these guidelines:\1. The response should include 5 sections: Title, Ingredient List, Step-by-Step Instructions, Expected Cooking Time, and Note. Limit your response to 150-200 words. 2. Return any message you are given as JSON.",
+      content: "As a recipe-generating assistant, create a recipe by using " +
+          selectedIngredients.join() +
+          " provided by the user. To ensure a precise and high-quality response, please follow these guidelines:\1. The response should include 5 sections: Title, Ingredient List, Step-by-Step Instructions, Expected Cooking Time, and Note. Limit your response to 150-200 words. \2. Return any message you are given as JSON.",
       role: OpenAIChatMessageRole.assistant,
     );
 
@@ -444,7 +443,7 @@ class CreateScreenState extends State<CreateScreen> {
     ];
     final completion = await OpenAI.instance.chat
         .create(model: 'gpt-3.5-turbo', messages: requestMessages);
-    print(completion.choices.first.message);
+    print(completion);
     setState(() {
       resultCompletion = completion.choices.first.message.content;
       // atomInputContainerController.clear();
